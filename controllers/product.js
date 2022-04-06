2933ad6cfba25f7ba22d20544bbed4bf1fed3933
duong19122002 <duong19122002@gmail.com>
2022-04-06 21:33:40 +0700

    save

--- controllers/product.js
+++ controllers/product.js
@@ -1,26 +1,26 @@
 import Product from '../models/product';
 //list sản phẩm
-export const detail =async (req, res) => {
-    const filter=req.query.id
+export const detail = async (req, res) => {
+    const filter = req.query.id
     console.log(filter)
     try {
-        const products=await Product.find({category:filter}).populate("category");
+        const products = await Product.find({ category: filter }).populate("category");
         res.json(products);
     } catch (error) {
         res.status(400).json({
-            message:"Không hien thi được sản phẩm anh ei"
+            message: "Không hien thi được sản phẩm anh ei"
         })
     }
 };
-export const list =async (req, res) => {
-  
+export const list = async (req, res) => {
+
     try {
         // const products=await Product.find().exec();
-        const products=await Product.find().populate("category").exec();
+        const products = await Product.find().populate("category").exec();
         res.json(products);
     } catch (error) {
         res.status(400).json({
-            message:"Không hien thi được sản phẩm anh ei"
+            message: "Không hien thi được sản phẩm anh ei"
         })
     }
 };
@@ -35,62 +35,61 @@ export const getByModel = async (req, res) => {
 }
 
 //post sản phẩm
-export const create =async (req, res) => {
+export const create = async (req, res) => {
     try {
         const product = await new Product(req.body).save();
-        
-        res.json(product)   
+
+        res.json(product)
     } catch (error) {
         res.status(400).json({
             message: "Không thêm được sản phẩm anh ei"
         })
     }
-//   products.push(req.body);
-//   res.json(products);
+    //   products.push(req.body);
+    //   res.json(products);
 };
 
-export const read = async(req, res) => {
+export const read = async (req, res) => {
     try {
-        const product=await Product.findOne({_id:req.params.id }).exec();
+        const product = await Product.findOne({ _id: req.params.id }).exec();
         res.json(product)
     } catch (error) {
         res.status(400).json({
-            message:"Hiển thị dữ liêu không thành công"
+            message: "Hiển thị dữ liêu không thành công"
         })
     }
-//   res.json(products.find((item) => item.id === +req.params.id));
+    //   res.json(products.find((item) => item.id === +req.params.id));
 };
-export const remove =async (req, res) => {
+export const remove = async (req, res) => {
     try {
-        const product = await Product.findOneAndDelete({_id:req.params.id}).exec();
+        const product = await Product.findOneAndDelete({ _id: req.params.id }).exec();
         res.json(product)
     } catch (error) {
         res.status(400).json({
-            error:"Xoá không thành công"
+            error: "Xoá không thành công"
         })
     }
-//   res.json(products.filter((item) => item.id === +req.params.id));
+    //   res.json(products.filter((item) => item.id === +req.params.id));
 };
-export const update= async(req, res) => {
-    const codition ={_id:req.params.id}
+export const update = async (req, res) => {
+    const codition = { _id: req.params.id }
     const update = req.body
 
-try {
-    const product= await Product.findOneAndUpdate(codition,update,{new:true}).exec();
-    res.json(product)
-    
-} catch (error) {
-    res.status(400).json({
-        error:"Xoá không thành công"
-    })
-}
+    try {
+        const product = await Product.findOneAndUpdate(codition, update, { new: true }).exec();
+        res.json(product)
+
+    } catch (error) {
+        res.status(400).json({
+            error: "Xoá không thành công"
+        })
+    }
 
     // res.json(products.map((item) => item.id ==req.params.id ? req.body :item));
-  };
+};
 
- export const search=async(req,res)=>{
-  const searchString= req.query.q ? req.query.q : "";
-  const result = await Product.find({$text : {$search:searchString}}).exec();
-  res.json(result)
-  }
-  
+export const search = async (req, res) => {
+    const  searchString = req.query.q ? req.query.q : "";
+    const result = await Product.find({ $text: { $search: searchString } }).exec();
+    res.json(result)
+}
--- controllers/user.js
+++ controllers/user.js
@@ -1,69 +1,69 @@
 
 import User from "../models/user";
-export const userbyId=async(req,res,next,id)=>{
+export const userbyId = async (req, res, next, id) => {
     try {
-        const user=await User.findById(id).exec();
-        if(!user){
+        const user = await User.findById(id).exec();
+        if (!user) {
             res.status(400).json({
                 message: "Không tìm thấy user"
             })
         }
-        req.profile=user;
+        req.profile = user;
         next();
     } catch (error) {
-        
+
     }
 }
-export const list =async(req,res)=>{
-   
+export const list = async (req, res) => {
+
     try {
-        const user=await User.find();
+        const user = await User.find();
         res.json(user)
     } catch (error) {
         res.status(400).json({
-            message:"Không hiển thị danh sách user"
+            message: "Không hiển thị danh sách user"
         })
     }
 }
-export const creat =async (req,res)=>{
+export const creat = async (req, res) => {
     try {
         console.log(req.body)
-        const post=await User(req.body).save();
-        
-        res.json(post) 
+        const post = await User(req.body).save();
+
+        res.json(post)
     } catch (error) {
         res.status(400).json({
             message: "Không thêm được user"
         })
     }
 }
-export const read = async(req,res)=>{
+export const read = async (req, res) => {
     try {
-        const post=await User.findOne({_id:req.params.id}).exec();
+        const post = await User.findOne({ _id: req.params.id }).exec();
         res.json(post)
     } catch (error) {
         res.status(400).json({
-            message:"Không hiển thị được user"
+            message: "Không hiển thị được user"
         })
     }
 }
-export const remove= async(req,res)=>{
+export const remove = async (req, res) => {
     try {
-        const post=await User.findOneAndDelete({_id:req.params.id}).exec();
+        const post = await User.findOneAndDelete({ _id: req.params.id }).exec();
         res.json(post)
     } catch (error) {
         res.status(400).json({
-            message:"Không xoa được user"
+            message: "Không xoa được user"
         })
     }
 }
-export const update=async(req,res)=>{
+export const update = async (req, res) => {
     try {
-        const post = await User.findByIdAndUpdate({_id:req.params.id},req.body,{new:true}).exec();
+        const post = await User.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true }).exec();
         res.json(post)
     } catch (error) {
         res.status(400).json({
-            message:"Không cap nhat được user"
+            message: "Không cap nhat được user"
         })
     }
 }
